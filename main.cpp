dda567c1e12d1b0f9233b6cc113621195b06ba14
Cruvadio <ialexandrova53@gmail.com>
2020-03-04 17:11:24 +0300

    added CMakeLists.txt to make compile_commands.json and some libraries

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+int main (int argc, char** argv)
+{
+  if (!glfwInit())
+  {
+    fprintf(stderr, "Error while initializing glfw");
+    return 1;
+  }
+
+  
+}
